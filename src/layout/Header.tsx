524d798f56e087e8eb4753d9fdc43cd9d4ca8bb1
79gun79 <79gun79@gmail.com>
2025-07-04 21:27:12 +0900

    feat: 라우팅마다 헤더 배경 변경

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -5,7 +5,10 @@ export default function Header() {
   return (
     <>
       <header className="flex h-24 items-center justify-center">
-        <img className="ml-5 h-24 w-24" src={Logo} alt="로고" />
+        <Link to="/">
+          <img className="ml-5 h-24 w-24" src={Logo} alt="로고" />
+        </Link>
+
         <div className="flex-grow"></div>
         <div className="mr-15 flex gap-11">
           <Link to="/about" className="h3 text-main">
